2de4e58a6592ea376d24c3feb1e0fc0c75038751
Marianela Cortina <marianelacort@hotmail.com>
2024-04-17 15:43:21 -0300

    changes in header

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+   public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_zk2hb0b', 'template_dht42s5', e.target as HTMLFormElement, {
+        publicKey: 'fc_xViW_Ezu-0ot6m',
+      })
+      .then(
+        () => {
+          alert('Mensaje enviado exitosamente!');
+          location.reload(); 
+        },
+        (error: any) => {
+          console.log('Algo falló...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  } 
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,15 +3,18 @@ import { HomeComponent } from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ HomeComponent, ContactComponent, PortfolioComponent],
+  imports: [ HomeComponent, ContactComponent, PortfolioComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
+
 
   constructor(
     private router: Router,
@@ -23,8 +26,12 @@ export class HeaderComponent {
   addToggle(){
     this.status = !this.status;       
   }
-
-
+/* 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+ */
+/* 
   onHome(){
     this.router.navigate(['/']);
   }
@@ -36,7 +43,7 @@ export class HeaderComponent {
   onPortfolio(){
     this.router.navigate(['/portfolio']);
   }
-
+ */
 
 
 
