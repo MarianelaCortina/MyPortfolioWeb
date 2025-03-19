470598391ade9f2897fc23402c4b3f72c50767b0
Marianela Cortina <marianelacort@hotmail.com>
2025-03-19 12:30:52 -0300

    Changes in the footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { ContactComponent } from '../contact/contact.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink} from '@angular/router';
 
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactComponent],
+  imports: [ContactComponent, RouterLink, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  menuOption: string = '';
+
+  onOption(menuOption: string){
+    this.menuOption = menuOption;
+  }
   
 }
